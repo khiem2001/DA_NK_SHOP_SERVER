a09217bff57dd566dafc95f162ade8e13f115b63
khiemnv <ngkhiem22072001@gmail.com>
2023-12-31 00:41:56 +0700

    fix

--- apps/notification-service/src/notification/cqrs/commands/handlers/create-notification.handler.ts
+++ apps/notification-service/src/notification/cqrs/commands/handlers/create-notification.handler.ts
@@ -1,8 +1,5 @@
 import { NotificationEntity } from '@app/core/entities/notification';
-import {
-  NotificationServiceClient,
-  NOTIFICATION_SERVICE_NAME,
-} from '@app/proto-schema/proto/notification.pb';
+
 import { Inject, Logger } from '@nestjs/common';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { ClientGrpc } from '@nestjs/microservices';
@@ -14,20 +11,8 @@ export class CreateNotificationHandler
   implements ICommandHandler<CreateNotificationCommand>
 {
   logger = new Logger(this.constructor.name);
-  private notificationService: NotificationServiceClient;
 
-  constructor(
-    private notificationRepository: NotificationRepository,
-    @Inject(NOTIFICATION_SERVICE_NAME)
-    private readonly notificationClient: ClientGrpc,
-  ) {}
-
-  onModuleInit() {
-    this.notificationService =
-      this.notificationClient.getService<NotificationServiceClient>(
-        NOTIFICATION_SERVICE_NAME,
-      );
-  }
+  constructor(private notificationRepository: NotificationRepository) {}
 
   async execute(command: CreateNotificationCommand): Promise<any> {
     const { cmd } = command;
