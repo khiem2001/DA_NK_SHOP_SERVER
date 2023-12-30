03fc0a93056e4b163c7ce0ac32b857809b8069b4
khiemnv <ngkhiem22072001@gmail.com>
2023-12-30 22:35:51 +0700

    fix

--- apps/gateway/src/mailer/input/mailer.input.ts
+++ apps/gateway/src/mailer/input/mailer.input.ts
@@ -1,14 +1,14 @@
 import { Field, InputType } from '@nestjs/graphql';
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import {  IsNotEmpty } from 'class-validator';
 @InputType()
 export class SendPinCodeInput {
   @Field(() => String)
   @IsNotEmpty({
     message: 'Vùng lòng nhập email',
   })
-  @IsEmail({
-    message: 'Email không đúng định dạng',
-  })
+  // @IsEmail({
+  //   message: 'Email không đúng định dạng',
+  // })
   email: string;
 }
 
